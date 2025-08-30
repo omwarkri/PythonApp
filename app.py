669f3f7ec5aea13c98f33f930b364c29bd5b43ee
omwarkri <owarkri710@gmail.com>
2025-08-30 08:49:56 +0530

    contineous loop add

--- app.py
+++ app.py
@@ -2,3 +2,8 @@ print("Hello from Om's Learning Project!")
 print("This is a simple Python application.")
 print("This is a new line we added through cludshel in guthub")
 print("Hello, everyone, myself Om")
+
+
+import time
+while True:
+    time.sleep(60) 
