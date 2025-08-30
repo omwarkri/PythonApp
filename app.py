3f118a08fd89c2c07c5e8662f594f75d1bf312e2
omwarkri <cloudshell-user@ip-10-133-56-76.eu-north-1.compute.internal>
2025-08-30 02:47:19 +0000

    New lines Added through cloudshell

---
+++ app.py
@@ -0,0 +1,4 @@
+print("Hello from Om's Learning Project!")
+print("This is a simple Python application.")
+print("This is a new line we added through cludshel in guthub")
+print("Hello, everyone, myself Om")
